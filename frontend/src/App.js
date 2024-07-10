b1b6683a94ae0e8f4147f40f313ff681e68b72e3
Cristian Teran <cristianteranv@gmail.com>
2024-07-09 20:07:57 -0400

    added material ui to app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { AppBar, Toolbar, Typography, Container, Button } from '@mui/material';
 import TicketForm from './components/TicketForm';
 
 const TicketList = () => {
@@ -19,22 +20,19 @@ const TicketList = () => {
 function App() {
   return (
     <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Submit Ticket</Link>
-            </li>
-            <li>
-              <Link to="/admin">Admin Panel</Link>
-            </li>
-          </ul>
-        </nav>
-        <Routes>
-          <Route exact path='/' element={<TicketForm/>} />
-          <Route exact path='/admin' element={<TicketList/>} />
-        </Routes>
-      </div>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6">
+            Help Desk
+          </Typography>
+          <Button color="inherit" component={Link} to="/">Ticket Form</Button>
+          <Button color="inherit" component={Link} to="/admin">Admin Panel</Button>
+        </Toolbar>
+      </AppBar>
+      <Routes>
+        <Route path="/" element={<TicketForm />} />
+        <Route path="/admin" element={<TicketList />} />
+      </Routes>
     </Router>
   );
 }
